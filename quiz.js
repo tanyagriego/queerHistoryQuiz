7f039dd33f99404977e5133d82f2db197f04896a
tanyagriego <tanya.griego@gmail.com>
2018-09-04 22:23:38 -0700

    Added style to front page

--- quiz.js
+++ quiz.js
@@ -20,8 +20,8 @@ let currentQuestionObject;
 
     //This poplulates the header and instructions upon page load
     $("header").text("Queer History");
-    $(".instructions").text("Some compelling text here.");
-    // $(".coverImage").attr("src","images/cover.jpg");;
+    $(".instructions").text("All movements have a history. Learn about ours here.");
+    $(".coverImage").attr("src","images/cover.jpg");;
 
     function hideQuizData () {
         $("#question").hide();
